dc81ea4df8cf52730c02f461fb4a42283fc74d0a
Amine Moumi <moumi.amine1@gmail.com>
2024-10-17 15:02:07 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'] 
+  
 })
 export class AppComponent {
-  title = 'playing-cards';
+ 
 }
